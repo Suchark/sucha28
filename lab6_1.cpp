c6654e85f681dfbe6eda5941bc037e36000ead64
Shuchanuch Rinkum <Suchanuch28@gmail.com>
2021-11-26 16:35:19 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 
 int main(){
+
+    int p;
+    int even = 0;
+    int odd = 0;
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> p;
+    while (p != 0)
+    {
+        if (p % 2 == 0){
+            even += 1;
+        }
+        else {
+            odd += 1;
+        }
+        cout << "Enter an integer: ";
+        cin >> p;
+
+    }
+    
+
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd ;
     return 0;
 }
